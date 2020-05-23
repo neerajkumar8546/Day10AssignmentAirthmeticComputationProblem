76a6ac8d32b3e40bd207fe8077692ce3985a8e53
unknown <neerajkumar8546@gmail.com>
2020-05-23 17:37:50 +0530

    Take Three input from user

---
+++ airthmetic_computation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+
+echo "Welcome to the Arithmetic Computations Program"
+
+
+#Take Three input
+ 	read -p "Enter first Number" a
+   read -p "Enter Second Number" b
+   read -p "Enter Third Number" c
+
+	echo "Taken Inputs are = " $a " " $b " " $c
